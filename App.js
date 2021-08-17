3023636a8534ce7a3f41a3da16f35c535c70d48c
Maxime Besson <maxime>
2021-08-17 15:19:46 +0200

    still trying to fix login username pushtoken

--- App.js
+++ App.js
@@ -360,15 +360,17 @@ function Login({ navigation }) {
 
                         fetch("http://91.121.143.104:7070/login", { signal: controller.signal, method: "POST", body: JSON.stringify({ "username": userName, "password": password }) }).then(r => {
                             if (r.status == 200) {
-                                username = userName; navigation.navigate('Home', { refresh: "refresh" });
-                                pushtoken(route.params.pushtoken, userName)
+                                username = userName; 
+                                pushtoken(route.params.pushtoken, userName);
+                                navigation.navigate('Home', { refresh: "refresh" });
+                                
                                 return;
                             }
                             else {
                                 alert("Wrong login or password!");
                                 return;
                             }
-                        }).catch(() => { console.log("Issue with server!"); return })}>
+                        }).catch((err) => { console.log(err, "Issue with server!"); return })}>
                     </Button>
                     {/* <Button style={{ margin: 30 }} color='grey' title="Register" onPress={() =>
 
