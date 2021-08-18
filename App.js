93eefd4377d3e7b6de63dc38a08161964142d1ba
Maxime Besson <maxime>
2021-08-18 15:03:59 +0200

    less warning still

--- App.js
+++ App.js
@@ -455,7 +455,7 @@ function SummaryScreen() {
 
 
                 return (
-                    <View style={{ flexDirection: "row", justifyContent: "flex-start", borderBottomWidth: 1, borderColor: "lightgrey" }}>
+                    <View key={r.name} style={{ flexDirection: "row", justifyContent: "flex-start", borderBottomWidth: 1, borderColor: "lightgrey" }}>
                         <View style={{ flexDirection: "row", width: 100 }}>
                             <Text style={styles.medailleNumber}>{r.rank + addth(r.rank)}</Text>
                         </View>
