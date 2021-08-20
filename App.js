e946a56aff9d28c8ef1553b677c76c0ef9fc4fa2
Antoine Mureddu <antoine.mureddu@softathome.com>
2021-08-20 13:54:11 +0200

    fix ios video + add legit review

--- App.js
+++ App.js
@@ -640,6 +640,11 @@ function VanRommelScreen() {
             </View>
             <View style={{ alignItems: "center" }}><Text style={{ fontSize: 16 }}> </Text></View>
             <View style={{ alignItems: "center" }}><Image style={{ width: "82%", height: 164 }} source={require('./assets/williwaller2006.png')} /></View>
+            <View style={{ alignItems: "center" }}><Text style={{ fontSize: 16 }}> </Text></View>
+            <View style={{ alignItems: "center" }}><Text style={{ fontSize: 16 }}> </Text></View>
+            <Image style={{ borderRadius: 40, width: 50, height: 50, marginLeft: 20 }} source={require('./assets/PaulB.jpg')} />
+            <Text style={{ fontSize: 16, fontWeight: "bold", marginLeft: 80, marginTop: -33 }}> Paul B.:</Text>
+            <Text style={{ fontSize: 14, fontStyle: "italic", marginLeft: 150, marginTop: -25, color: "blue", textAlign: "center"}}> “Mon seul regret est de ne jamais avoir pu travailler avec les Van Rommel.”:</Text>
             <View style={{ flex: 1, alignContent: 'center', justifyContent: 'flex-start', flexDirection: "row" }}>
                 <View style={{ flex: 1 }}>
                     <View style={{ alignItems: "center" }}><Text style={{ fontSize: 16 }}> </Text></View>
@@ -647,6 +652,7 @@ function VanRommelScreen() {
                     <TouchableOpacity onPress={() => Linking.openURL('mailto:fritkotvanrommel@gmail.com')}>
                         <View style={{ alignItems: "center" }}><Text style={{ fontSize: 16, color: "blue", textDecorationLine: "underline" }}>Nous contacter</Text></View>
                     </TouchableOpacity>
+                    <View style={{ alignItems: "center" }}><Text style={{ fontSize: 16 }}> </Text></View>
 
                 </View>
                 <View style={{ flex: 1 }}>
@@ -655,6 +661,7 @@ function VanRommelScreen() {
                     <TouchableOpacity onPress={() => Linking.openURL('https://docs.google.com/forms/d/e/1FAIpQLSfNP_1o3R7emNIM9B-JFRfge6lWQuD_0gyflO3xorB0MNUaVg/viewform')}>
                         <View style={{ alignItems: "center" }}><Text style={{ fontSize: 16, color: "blue", textDecorationLine: "underline" }}>Nous rejoindre</Text></View>
                     </TouchableOpacity>
+                    <View style={{ alignItems: "center" }}><Text style={{ fontSize: 16 }}> </Text></View>
                 </View>
             </View>
         </ScrollView>
@@ -815,7 +822,7 @@ function videoScep(setScep, scep, video) {
                         ref={video}
                         style={{ width: "100%", height: "100%" }}
                         source={require("./assets/oss.mp4")}
-                        resizeMode="cover"
+                        resizeMode={Platform.OS === "ios" ? "contain" : "cover"}
                         isLooping={false}
                         onPlaybackStatusUpdate={status => { if (status.durationMillis == status.positionMillis && status.durationMillis) { setScep(false) } }}
 
