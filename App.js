dd8565a85c104361d12ca86d1b7756f8009a89c2
Antoine Mureddu <antoine.mureddu@softathome.com>
2021-08-24 12:42:10 +0200

    modify display

--- App.js
+++ App.js
@@ -1330,14 +1330,13 @@ function App() {
                                 title: "Home", headerRight: () => (
                                     <View style={{ flexDirection: "row", margin: 10 }}>
                                         <Pressable onPress={() => { setChat(true) }}>
-                                            <Image style={{ borderRadius: 15, width: 30, height: 30, backgroundColor: "white", marginRight: 10, alignSelf:"center" }} source={newMessage ? require('./assets/chatnewmessage.png') : require('./assets/chat.png')} />
+                                            <Image style={{ borderRadius: 15, width: 30, height: 30, backgroundColor: "white", marginRight: 110, marginTop: 25, alignSelf:"center" }} source={newMessage ? require('./assets/chatnewmessage.png') : require('./assets/chat.png')} />
                                         </Pressable>
                                         <TouchableOpacity onPress={playmegaphone}>
                                             <Image style={{ borderRadius: 40, width: 20, height: 20, margin: 30 }} source={require('./assets/megaphone.png')} />
+                                        </TouchableOpacity><TouchableOpacity style={{ alignContent: "center", textAlignVertical: "center" }} onPress={() => { navigation.navigate('UsernameScreen') }}>
+                                            <Text style={{ color: "white", marginTop: 30, alignSelf: "center", textAlignVertical: "center" }}>{username}</Text>
                                         </TouchableOpacity>
-                                        <View><TouchableOpacity style={{ alignContent: "center", textAlignVertical: "center" }} onPress={() => { navigation.navigate('UsernameScreen') }}>
-                                            <Text style={{ color: "white", margin: 10, alignSelf: "center", textAlignVertical: "center" }}>{username}</Text>
-                                        </TouchableOpacity></View>
                                     </View>)
                             })} name="Home" component={HomeScreen} />
 
@@ -1369,7 +1368,7 @@ function App() {
                             <Stack.Screen options={() => ({
                                 title: "Tableau des médailles", headerRight: () => <View>
                                     <Pressable onPress={() => { setChat(true) }}>
-                                        <Image style={{ borderRadius: 15, width: 30, height: 30, backgroundColor: "white", marginRight: 10 }} source={newMessage ? require('./assets/chatnewmessage.png') : require('./assets/chat.png')} />
+                                        <Image style={{ borderRadius: 15, width: 30, height: 30, backgroundColor: "white", marginRight: 20 }} source={newMessage ? require('./assets/chatnewmessage.png') : require('./assets/chat.png')} />
                                     </Pressable>
                                 </View>
                             })} name="SummaryScreen" component={SummaryScreen} />
@@ -1384,7 +1383,7 @@ function App() {
                             <Stack.Screen options={({ navigation }) => ({
                                 title: "Clicker!", headerRight: () => <View>
                                     <Pressable onPress={() => { setChat(true) }}>
-                                        <Image style={{ borderRadius: 15, width: 30, height: 30, backgroundColor: "white", marginRight: 10 }} source={newMessage ? require('./assets/chatnewmessage.png') : require('./assets/chat.png')} />
+                                        <Image style={{ borderRadius: 15, width: 30, height: 30, backgroundColor: "white", marginRight: 20 }} source={newMessage ? require('./assets/chatnewmessage.png') : require('./assets/chat.png')} />
                                     </Pressable>
                                 </View>
                             })} initialParams={{ username: username }} name="ClickerScreen" component={ClickerScreen} />
