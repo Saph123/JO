fd71698a6cad50035dc6a72c25156fd5f6519736
Max <max.saph@gmail.com>
2023-06-10 20:10:36 +0200

    update first draft shifumi

--- ShifumiScreen.js
+++ ShifumiScreen.js
@@ -20,23 +20,26 @@ export function ShifumiScreen({ route }) {
             <View style={{ flex: 1, flexDirection: "row", backgroundColor: "lightblue", width: "100%", borderColor: "black", borderWidth: 1 }}>
                 <Text>La partie commence dans 10 sec! Choisis un signe, gros porc!</Text>
             </View>
-            <View style={{ flex: 4, flexDirection: "column", width: "100%", borderColor: "black", borderWidth: 1, marginBottom:30 }}>
+            <View style={{ flex: 4, flexDirection: "column", width: "100%", borderColor: "black", borderWidth: 1, marginBottom: 30 }}>
                 <ScrollView style={{ flex: 1, width: "100%", alignSelf: "flex-start" }}>
                     <View style={{ flex: 1, width: "100%", flexDirection: "row", alignContent: "flex-start" }}>
                         <Text style={{ alignSelf: "flex-start", alignContent: "flex-start", backgroundColor: "black", color: "white", marginRight: 5, maxHeight: 20 }}>LaGuille:</Text><Text>Chat mes gatés</Text>
                     </View>
                 </ScrollView>
-                <View style={{ minHeight:30, flexDirection: "row" }}>
-                    <TextInput style={{ flex:1, borderColor: "black", borderWidth: 1 }}></TextInput>
+                <View style={{ minHeight: 30, flexDirection: "row" }}>
+                    <TextInput style={{ flex: 1, borderColor: "black", borderWidth: 1 }}></TextInput>
                     <Pressable onPress={() => { pushChat(sportname, localText, username); setChatText(text + "\n" + username + ":" + localText); setLocalText(""); }}>
                         <Image style={{ width: 50, height: 50 }} source={require('./assets/sendmessage.png')} />
                     </Pressable>
                 </View>
             </View>
             <View style={{ flex: 1, alignItems: "center", alignContent: "center", flexDirection: "row" }}>
-                <Pressable style={{ flex: 1, backgroundColor: "lightblue", borderColor: "black", borderWidth: 1, height: "100%", borderRadius:25, marginRight:10 }}><Text style={{flex:1, textAlignVertical:"center", textAlign:"center"}}>Pierre</Text></Pressable>
-                <Pressable style={{ flex: 1, backgroundColor: "lightblue", borderColor: "black", borderWidth: 1, height: "100%", borderRadius:25, marginRight:10 }}><Text style={{flex:1, textAlignVertical:"center", textAlign:"center"}}>Feuille</Text></Pressable>
-                <Pressable style={{ flex: 1, backgroundColor: "lightblue", borderColor: "black", borderWidth: 1, height: "100%", borderRadius:25, marginRight:10 }}><Text style={{flex:1, textAlignVertical:"center", textAlign:"center"}}>Ciseaux</Text></Pressable>
+                <Pressable style={{ flex: 1, backgroundColor: "lightblue", borderColor: "black", borderWidth: 1, height: "100%", borderRadius: 25, marginRight: 10 }}>
+                    <Image style={{ alignSelf:"center", tintColor: "black", height: 50, width:50, marginTop:5 }} resizeMode="contain" source={require('./assets/paper.png')} /></Pressable>
+                <Pressable style={{ flex: 1, backgroundColor: "lightblue", borderColor: "black", borderWidth: 1, height: "100%", borderRadius: 25, marginRight: 10 }}>
+                    <Image style={{ alignSelf:"center", tintColor: "black", height: 50, width:50, marginTop:5 }} resizeMode="contain" source={require('./assets/fist.png')} /></Pressable>
+                <Pressable style={{ flex: 1, backgroundColor: "lightblue", borderColor: "black", borderWidth: 1, height: "100%", borderRadius: 25, marginRight: 10 }}>
+                    <Image style={{ alignSelf:"center", tintColor: "black", height: 50, width:50, marginTop:5, transform: [{ rotate: '270deg'}] }} resizeMode="contain" source={require('./assets/scissors.png')} /></Pressable>
             </View>
         </View>
     )
