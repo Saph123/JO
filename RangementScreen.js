854bf29b9647e13e38462f99f50b998c92862318
Antoine Mureddu <antoine.mureddu@sailsense.io>
2024-07-08 22:46:50 +0200

    improve rangelent, needs saving now

--- RangementScreen.js
+++ RangementScreen.js
@@ -1,24 +1,23 @@
 import styles from "./style.js";
 import * as React from 'react';
-import { View, Pressable, Image, ScrollView, Text, TextInput, ActivityIndicator } from 'react-native';
-import { lutImg, vibrateLight, updateMission, fetchRangement } from './utils.js';
+import { View, Pressable, Image, ScrollView, Text, TextInput, ActivityIndicator, Modal } from 'react-native';
+import { lutImg, vibrateLight, fetchRangement } from './utils.js';
 import { adminlist } from "./global.js";
 export function RangementScreen({ route }) {
     const [loading, setLoading] = React.useState(true)
     const [tabs, setTab] = React.useState({ states: ["résumé"], status: "résumé" });
-    const [missions, setMissions] = React.useState([{ title: "" }])
-    const [currentMisson, setCurrentMission] = React.useState({ title: "" })
-    const [temp_mission, setTempMission] = React.useState("")
+    const [tasks, setTasks] = React.useState([{ title: "" }])
+    const [currentTask, setCurrentTask] = React.useState({ title: "" })
     const [shouldSave, setShouldSave] = React.useState(false)
-    const [motiv_text, setMotivText] = React.useState("")
     const [focus, setFocus] = React.useState(false)
-    let missions_list = missions;
+    const [displayPoints, setDisplayPoints] = React.useState(true)
+    let tasks_list = tasks;
 
 
     React.useEffect(() => {
         if (loading) {
             fetchRangement().then(r =>
-                setMissions(r)
+                setTasks(r)
             )
             setLoading(false);
             if (route.params.username in adminlist) {
@@ -43,14 +42,38 @@ export function RangementScreen({ route }) {
                         { flex: 1, backgroundColor: "black", borderColor: "grey", borderWidth: 5, alignItems: "center", borderBottomStartRadius: 15, borderBottomEndRadius: 15 }}><View>
                             <Image style={styles.tabimage} resizeMode="contain" resizeMethod="auto" source={lutImg(r)} /></View></Pressable>)}
             </View>
-
             <View>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={focus} style={{ paddingTop: "30%" }}>
+                    <View style={[styles.matchZoomView, { minHeight: 10 }]}>
+                        <Pressable style={[styles.closeButton, { marginBottom: 15 }]} onPress={() => { setFocus(false); setShouldSave(true) }}><Image style={{ alignSelf: "center", marginVertical: 4 }} resizeMode="cover" resizeMethod="resize" source={require('./assets/remove.png')} /></Pressable>
+                        <View style={{ flexDirection: "row", margin: 10 }}>
+                            <Text style={{ margin: 2 }}>Tache: </Text>
+                            <TextInput onChangeText={text => {currentTask.title = text}} autoFocus={currentTask.title == ""} style={{ borderWidth: 2 }}>{currentTask.title}</TextInput>
+
+                        </View>
+                        <View style={{ flexDirection: "row", margin: 10 }}>
+                            {displayPoints ? <Text style={{ alignSelf: "center", marginRight: 10 }}>Points: {currentTask.points}</Text> : <Text style={{ alignSelf: "center", marginRight: 10 }}>Points: {currentTask.points}</Text>}
+                            <View>
+                                <Pressable onPressIn={() => { currentTask.points++; setDisplayPoints(false) }} onPressOut={() => { setDisplayPoints(true) }}>
+                                    <Image source={require('./assets/simpleplus.png')} ></Image>
+                                </Pressable>
+                                <Pressable onPressIn={() => { if (currentTask.points > 1) currentTask.points--; setDisplayPoints(false) }} onPressOut={() => { setDisplayPoints(true) }}>
+                                    <Image style={{ transform: [{ rotate: '180deg' }] }} source={require('./assets/simpleplus.png')} ></Image>
+                                </Pressable>
+                            </View>
+                        </View>
+                    </View>
+
+                </Modal>
                 <View>
                     <View style={{ flexDirection: "row", marginTop: 22 }}>
                         <ScrollView style={{ flex: 1, borderRightWidth: 1, borderColor: "#E0E0E0" }}>
                             <Text style={[styles.showPlayers, { height: 60, width: 200, fontWeight: "bold", fontSize: 18 }]}>Taches</Text>
-                            {missions_list.map(r =>
-                                <Pressable key={r.title} onPress={() => { setTempMission(r.title); setCurrentMission(r); setFocus(true) }}>
+                            {tasks_list.map(r =>
+                                <Pressable key={r.title} onPress={() => { console.log("test"); setCurrentTask(r); setFocus(true) }}>
                                     <Text key={r.title} style={{ minHeight: 30, width: 200, textAlignVertical: "center", textAlign: "left", padding: 5, paddingRight: 35, borderWidth: 1, borderColor: "#E0E0E0", marginLeft: 5 }}>{r.title}</Text>
                                 </Pressable>
                             )
@@ -60,14 +83,6 @@ export function RangementScreen({ route }) {
                         <View style={{ flex: 1 }}>
                             <View style={{ width: 100, height: 60, backgroundColor: shouldSave ? "red" : "white", justifyContent: "center", borderRadius: 30, borderWidth: 2, marginLeft: 5, marginBottom: 5 }}>
                                 <Pressable style={({ pressed }) => [{ opacity: pressed ? 0.2 : 1, alignSelf: "center" }]} onPress={() => {
-                                    updateMission(missions_list, route.params.username, setRefresh, setShouldSave);
-                                    let nb_missions = missions_list.length
-                                    for (let index = 0; index < missions_list.length; index++) {
-                                        if (missions_list[index].title == "") {
-                                            nb_missions--;
-                                        }
-                                    }
-                                    setMotivText(nb_missions + "/25")
                                 }
                                 }>
                                     <Text>Sauvegarder</Text>
@@ -75,10 +90,9 @@ export function RangementScreen({ route }) {
                             </View>
                             <View style={{ width: 100, height: 60, backgroundColor: "white", justifyContent: "center", borderRadius: 30, borderWidth: 2, marginLeft: 5, marginBottom: 5 }}>
                                 <Pressable style={({ pressed }) => [{ opacity: pressed ? 0.2 : 1, alignSelf: "center" }]} onPress={() => {
-                                    let missions = missions_list;
-                                    missions.push({ title: "" })
-                                    setTempMission("");
-                                    setCurrentMission(missions[missions.length - 1]);
+                                    let tasks = tasks_list;
+                                    tasks.push({ title: "", points: 1, participants: [], done: false })
+                                    setCurrentTask(tasks[tasks.length - 1]);
                                     setFocus(true)
                                 }
                                 }>
